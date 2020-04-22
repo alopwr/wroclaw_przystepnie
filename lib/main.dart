e55951ce2bfef97bfab4dd0df48b52394a1b7ce9
simon-the-shark <simon_rekin@o2.pl>
2020-04-22 12:05:19 +0200

    added sentry integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sentry/sentry.dart';
 import 'package:wroclaw_przystepnie/widgets/back_button_manager.dart';
 
 import 'providers/auth.dart';
@@ -8,8 +11,50 @@ import 'providers/tracks.dart';
 import 'providers/user_location.dart';
 import 'screens/login_screen.dart';
 import 'screens/map_screen.dart';
+import 'sentry_message.dart';
 
-void main() => runApp(MyApp());
+final SentryClient _sentry = SentryClient(
+    dsn:
+        "https://07feea34da564fff9da2e62c2396e157@o367012.ingest.sentry.io/5205866");
+
+bool get isInDebugMode {
+  bool inDebugMode = false;
+  assert(inDebugMode = true);
+  return inDebugMode;
+}
+
+Future<Null> _reportSentryError(dynamic error, dynamic stackTrace) async {
+  print('Caught error: $error');
+  if (isInDebugMode) {
+    print(stackTrace);
+    print('In dev mode. Not sending report to Sentry.io.');
+    return;
+  }
+  print('Reporting to Sentry.io...');
+  final Event event = await getSentryEnvEvent(error, stackTrace);
+  final SentryResponse response = await _sentry.capture(event: event);
+
+  if (response.isSuccessful) {
+    print('Success! Event ID: ${response.eventId}');
+  } else {
+    print('Failed to report to Sentry.io: ${response.error}');
+  }
+}
+
+void main() {
+  FlutterError.onError = (FlutterErrorDetails details) async {
+    if (isInDebugMode) {
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      Zone.current.handleUncaughtError(details.exception, details.stack);
+    }
+  };
+  runZoned<Future<Null>>(() async {
+    runApp(MyApp());
+  }, onError: (error, stackTrace) async {
+    await _reportSentryError(error, stackTrace);
+  });
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
---
+++ lib/sentry_message.dart
@@ -0,0 +1,68 @@
+import 'dart:io';
+
+import 'package:device_info/device_info.dart';
+import 'package:sentry/sentry.dart';
+
+final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+
+Future<Event> getSentryEnvEvent(dynamic exception, dynamic stackTrace) async {
+  /// return Event with IOS extra information to send it to Sentry
+  if (Platform.isIOS) {
+    final IosDeviceInfo iosDeviceInfo = await deviceInfo.iosInfo;
+    return Event(
+      release: '1.0.0',
+      environment: 'production', // replace it as it's desired
+      extra: <String, dynamic>{
+        'name': iosDeviceInfo.name,
+        'model': iosDeviceInfo.model,
+        'systemName': iosDeviceInfo.systemName,
+        'systemVersion': iosDeviceInfo.systemVersion,
+        'localizedModel': iosDeviceInfo.localizedModel,
+        'utsname': iosDeviceInfo.utsname.sysname,
+        'identifierForVendor': iosDeviceInfo.identifierForVendor,
+        'isPhysicalDevice': iosDeviceInfo.isPhysicalDevice,
+      },
+      exception: exception,
+      stackTrace: stackTrace,
+    );
+  }
+
+  /// return Event with Andriod extra information to send it to Sentry
+  if (Platform.isAndroid) {
+    final AndroidDeviceInfo androidDeviceInfo = await deviceInfo.androidInfo;
+    return Event(
+      release: '1.0.0',
+      environment: 'production', // replace it as it's desired
+      extra: <String, dynamic>{
+        'type': androidDeviceInfo.type,
+        'model': androidDeviceInfo.model,
+        'device': androidDeviceInfo.device,
+        'id': androidDeviceInfo.id,
+        'androidId': androidDeviceInfo.androidId,
+        'brand': androidDeviceInfo.brand,
+        'display': androidDeviceInfo.display,
+        'hardware': androidDeviceInfo.hardware,
+        'manufacturer': androidDeviceInfo.manufacturer,
+        'product': androidDeviceInfo.product,
+        'version': androidDeviceInfo.version.release,
+        'supported32BitAbis': androidDeviceInfo.supported32BitAbis,
+        'supported64BitAbis': androidDeviceInfo.supported64BitAbis,
+        'supportedAbis': androidDeviceInfo.supportedAbis,
+        'isPhysicalDevice': androidDeviceInfo.isPhysicalDevice,
+      },
+      exception: exception,
+      stackTrace: stackTrace,
+    );
+  }
+
+  /// Return standard Error in case of non-specifed paltform
+  ///
+  /// if there is no detected platform,
+  /// just return a normal event with no extra information
+  return Event(
+    release: '1.0.0',
+    environment: 'production',
+    exception: exception,
+    stackTrace: stackTrace,
+  );
+}
